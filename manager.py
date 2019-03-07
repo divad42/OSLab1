318e395f958366a53bfebfca7a518b881a4cc135
astroengisci <andrewh.5299@gmail.com>
2019-03-07 12:09:46 -0600

    Foop

--- manager.py
+++ manager.py
@@ -2,30 +2,32 @@ class Manager:
     """The actual class to perform the simulation. Each entry in the partition table is [size, job]. If the partition is not in use, job = -1. The job table is a dictionary storing [partition, timeRemaining]. Entries are deleted once the job completes."""
     parttable = []
     jobtable = {}
-    def __init__(self, jobs, parts):
-        self.jobs = jobs
+    def __init__(self, parts):
         for part in parts:
             size = int(part[1])
-            parttable.append([size, -1])
-
+            self.parttable.append([size, -1])
 
     def fitjob(self, job):
-        for i in range(len(parttable)):
-            if(parttable[i][1] == -1 and parttable[i][0] >= int(job[2])):
-                jobtable[job[0]] = [i, job[1]]
-                parttable[i][1] = job[0]
+        if job[0] in self.jobtable.keys():
+            raise ValueError("Job number " + str(job[0]) + " already exists.")
+        for i in range(len(self.parttable)):
+            if(self.parttable[i][1] == -1 and self.parttable[i][0] >= int(job[2])):
+                self.jobtable[job[0]] = [i, job[1]]
+                self.parttable[i][1] = job[0]
                 return True
 
         return False
-            this.parttable.append([size, -1])
-
 
     def tick(self):
+        deletion = []
         for key in self.jobtable.keys():
             job = self.jobtable[key]
             # Reduce time remaining
             job[1] -= 1
             if job[1] == 0: # Job is finished
-                # Set the job's partition to be free and clear the entry from jobtable
-                self.parttable[job[0]][1] = -1
-                del self.jobtable[key]
+                deletion.append(key)
+        for key in deletion:
+            job = self.jobtable[key]
+            # Set the job's partition to be free and clear the entry from jobtable
+            self.parttable[job[0]][1] = -1
+            del self.jobtable[key]
