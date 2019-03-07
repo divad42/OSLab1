60f64be0650cf60d3862f93af94a3662da4c7778
astroengisci <andrewh.5299@gmail.com>
2019-03-07 11:38:53 -0600

    Add tick() and fix broken syntax in constructor

--- manager.py
+++ manager.py
@@ -5,6 +5,16 @@ class Manager:
     def __init__(self, jobs, parts):
         self.jobs = jobs
         for part in parts:
-            print(part)
             size = int(part[1])
-            parttable.append([size, -1])
+            this.parttable.append([size, -1])
+
+    def tick(self):
+        for key in self.jobtable.keys():
+            job = self.jobtable[key]
+            # Reduce time remaining
+            job[1] -= 1
+            if job[1] == 0: # Job is finished
+                # Set the job's partition to be free and clear the entry from jobtable
+                self.parttable[job[0]][1] = -1
+                del self.jobtable[key]
+                
