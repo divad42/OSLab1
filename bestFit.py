6ae68e9d38a466eda5134ee3996c90df2b70f039
David Carey <david@Davids-MacBook-Pro.local>
2019-03-11 16:49:22 -0500

    Implemented the manager in bestFit.py

--- bestFit.py
+++ bestFit.py
@@ -1,8 +1,25 @@
 import csv
+from manager import Manager
 
 with open('jobtable.csv', 'r') as jobfile:
     with open('sortedpartitions.csv', 'r') as partfile:
-       jobs = csv.reader(jobfile, delimiter=',', quotechar='"')
-       parts = csv.reader(partfile, delimiter=',', quotechar='"')
-       for part in parts:
-           print(part)
+        jobs = csv.reader(jobfile, delimiter=',', quotechar='"')
+        parts = csv.reader(partfile, delimiter=',', quotechar='"')
+
+        partitions = []
+
+        for part in parts:
+            partitions.append(part)
+           
+        man = Manager(partitions)
+
+        for job in jobs:
+            wait = 0
+            while not (man.fitjob(job)):
+                man.tick()
+                wait += 1
+            
+            print("Fit job " + job[0] + " after " + str(wait) + " ticks.")
+
+        print("done")
+
